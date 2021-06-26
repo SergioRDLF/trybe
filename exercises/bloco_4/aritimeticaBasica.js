00fd7bbff05daa424d1dd022e70169cd9eefaf01
Sergio_Rodrigues <rodrigues.sergiofilho@gmail.com>
2021-06-26 17:19:16 -0300

    Dia 4.1 Exercicio #1

---
+++ exercises/bloco_4/aritimeticaBasica.js
@@ -0,0 +1,22 @@
+let a = 10;
+let b = 30;
+
+// Adicao
+let adicao = a + b;
+console.log(adicao);
+
+//Subtração
+let subtracao = a - b;
+console.log (subtracao);
+
+//multiplicao
+let multiplicacao = a * b;
+console.log(multiplicacao);
+
+//Divisao
+let divisao = a/b;
+console.log(divisao);
+
+//Modulo
+let modulo = a % b;
+console.log(modulo);
