e4c1b497cb1cb5f903c449fc892cd01d1d75c890
Sergio_Rodrigues <rodrigues.sergiofilho@gmail.com>
2021-06-26 20:54:30 -0300

    Ex #6

---
+++ exercises/bloco_4/dia_1/exercicio6.js
@@ -0,0 +1,26 @@
+let peca = "rei"
+
+// Copiado da parte de gabarito!
+switch (peca.toLowerCase()) {
+    case 'rei':
+    console.log('Rei-> Uma casa apenas para qualquer direção.');
+    break;
+  case 'bispo':
+    console.log('Bispo-> Diagonal.');
+    break;
+  case 'rainha':
+    console.log('Rainha-> Diagonal, horizontal e vertical.');
+    break;
+  case 'cavalo':
+    console.log('Cavalo -> "L" pode pular sobre as peças.');
+    break;
+  case 'torre':
+    console.log('Torre -> Horizontal e vertical.');
+    break;
+  case 'peão':
+    console.log("Peão -> Apenas uma casa para frente, no primeiro movimento podem ser duas casas.");
+    break;
+  default:
+    console.log('Erro, peça inválida!');
+    break;
+}
