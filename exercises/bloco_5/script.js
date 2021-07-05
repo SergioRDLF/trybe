e11ed480793edc611853e1604326fc390705f61a
Sergio_Rodrigues <rodrigues.sergiofilho@gmail.com>
2021-07-05 16:01:05 -0300

    Adicionando o bloco 5

---
+++ exercises/bloco_5/script.js
@@ -0,0 +1,3 @@
+let element = document.querySelector('elementoOndeVoceEsta');
+document.querySelector('elementoOndeVoceEsta').parentNode.collor = red;
+document.querySelector('primeiroFilhoDoFilho').innerText = 'LALALALALA';
