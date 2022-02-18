9653f25758fdbd4a69316f9e956fcc04ba5395b8
Sergio_Rodrigues <rodrigues.sergiofilho@gmail.com>
2022-02-18 15:04:54 -0300

    Adicionando enum

---
+++ exercises/back_end/exmplos_course/bloco_26/exercises1/enum.ts
@@ -0,0 +1,34 @@
+enum weekDays {
+  sun = 1,
+  mon,
+  tue,
+  wed,
+  thu,
+  fri,
+  sat
+}
+
+enum rainbow {
+  vermelho = 'vermelho',
+  laranja = 'laranja',
+  amarelo = 'amarelo',
+  verde = 'verde',
+  azul = 'azul',
+  anil = 'anil',
+  violeta = 'violeta'
+}
+
+enum action {
+  salvar,
+  imprimir,
+  abrir,
+  visualizar,
+  fechar
+}
+
+enum pontosCardeais {
+  norte = "N",
+  leste = "L",
+  sul = "S",
+  oeste = "O",
+}
